9a4a56155cea567e44f2d85844ad7630c4cec3d9
Aliasghar Hasanzadeh <a.hasanzadeh2006@gmail.com>
2026-02-13 22:19:22 +0330

    Update app.js

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ const sources = [
         if (!data || (data.code != 0 && data.code != 200) || !data.data) {
           throw new Error(`پاسخ نامعتبر از سرور (Code: ${data?.code})`);
         }
-        let rawPrice = data.data.price18;
+        let rawPrice = data.data.price18 + '00';
         if (typeof rawPrice === "string") rawPrice = rawPrice.replace(/,/g, "").trim();
         return {
           price: Number(rawPrice),
@@ -271,3 +271,4 @@ function init() {
 }
 
 document.addEventListener("DOMContentLoaded", init);
+
